19783ce431bf916eb163d25a65617df757d8e2b0
madushaperera07 <madushanilakshan9@gmail.com>
2023-09-22 20:51:52 +0530

    new commit

--- client/src/Components/Events.js
+++ client/src/Components/Events.js
@@ -8,41 +8,16 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 
 
 function Events() {
-
-  const [events , setEvents] = useState([])
-
-  useEffect(()=>{
-    async function displayEvent(){
-      try{
-        const response = await axios.get("http://localhost:5000/api/events/details")
-        setEvents(response.data)
-      }catch(err){
-        console.log(err)
-      }
-    }
-    displayEvent()
-  },[])
-
-
-
-
   return (
     <div>
       <div>
         <Header />
       </div>
       <div>
-        <FullCalendar
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
-          events={events.map((event)=>({
-            titel : event.titel,
-            start : event.start,
-            end : event.end,
-            location : event.location,
-            note : event.note
-          }))}
-        />
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
       </div>
       <div>
         <Footer />
